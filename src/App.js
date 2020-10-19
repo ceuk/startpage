b7f3d8292574e6d092b67700f516c605ef4b94e0
ceuk <ceuk.dev@gmail.com>
2020-10-19 18:34:30 +0100

    update github api url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ProductHunt from './components/Feed/ProductHunt'
 function App () {
   return (
     <div className="App" style={{ display: 'flex' }}>
-      <Github title="Github" url="https://github-trending-api.now.sh" />
+      <Github title="Github" url="https://gtrend.yapie.me/" />
       <Reddit title="Reddit" url="https://www.reddit.com/user/calvers70/m/all.json" />
       <ProductHunt title="Product_Hunt" url="https://www.producthunt.com/feed?category=undefined" />
     </div>

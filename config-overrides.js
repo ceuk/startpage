24e331ad61a2454112ae552bb8bca1caa6b7841b
ceuk <ceuk.dev@gmail.com>
2022-07-01 16:39:22 +0100

    update colors

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
+module.exports = function override(config, env) {
+  config.plugins.push(new NodePolyfillPlugin())
+  return config
+}
